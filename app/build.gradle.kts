7c7f39ca90844aa8b7cf822f1bbce074f3c9150b
j-ritter <ritterj010@gmail.com>
2025-03-24 19:33:20 -0700

    dp instead of sp

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.ritter.smartstackbills"
         minSdk = 24
         targetSdk = 34
-        versionCode = 7
-        versionName = "1.2"
+        versionCode = 8
+        versionName = "1.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
